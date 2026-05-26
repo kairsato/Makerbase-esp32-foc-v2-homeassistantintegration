a9154231ce14f77dab7c1f56a775f96d50b78149
Kai Sato <kairobbiesato@gmail.com>
2026-05-27 01:40:23 +1000

    moved into __init__py

--- esphome/components/makerbase_foc/__init__.py
+++ esphome/components/makerbase_foc/__init__.py
@@ -1,4 +1,18 @@
-# Makerbase FOC ESPHome external component
-# Fan platform entrypoint
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import fan
+from esphome.const import CONF_ID
 
-from esphome.components.fan import Fan
+DEPENDENCIES = []
+
+makerbase_foc_ns = cg.esphome_ns.namespace("makerbase_foc")
+MakerbaseFOCFan = makerbase_foc_ns.class_("MakerbaseFOCFan", fan.Fan, cg.Component)
+
+CONFIG_SCHEMA = fan.FAN_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(MakerbaseFOCFan),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await fan.register_fan(var, config)
