f4767372787656dc0c0d575092151c8659325a17
Kai Sato <kairobbiesato@gmail.com>
2026-05-28 21:03:25 +1000

    add extra library

--- esphome/components/makerbase_foc/__init__.py
+++ esphome/components/makerbase_foc/__init__.py
@@ -13,6 +13,7 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
+    cg.add_library("SPI", None)
     cg.add_library("Simple FOC", "2.3.3")
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
